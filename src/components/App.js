e83e8ad56a89e4ca22d0b936579e6b8a9b8af547
shenyiling <jzx19951028yyjy@163.com>
2017-12-11 11:35:36 +0800

    first change

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+/**
+ * 测试react全家桶
+ * @authors Your Name (you@example.org)
+ * @date    2017-12-11 11:31:38
+ * @version $Id$
+ */
+
+import React from 'react'
+
+export default class App extends React.Component {
+	render() {
+		return (
+				<div>测试成功。</div>
+			);
+	}
+}
