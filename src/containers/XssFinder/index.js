8733e1daeaa9b03d4792dd7ddb6e09694dd24efc
shenyiling <jzx19951028yyjy@163.com>
2018-02-24 16:06:28 +0800

    feat: xss ui

--- src/containers/XssFinder/index.js
+++ src/containers/XssFinder/index.js
@@ -5,9 +5,20 @@ export default class XssFinder extends React.Component {
 
     render() {
         return (
-            <div className="xss-finder">
-                xss检测器
-            </div>
+            <section class="xss-container">
+                <h1> 
+                    为了更好地互联网 
+                    <a href="http://http://www.anquan.us/" target="_blank"> for better internet.</a> 
+                </h1>
+
+                <div class="search">
+                    <input type="text"/>    
+                    <div class="close">
+                        <span class="front"></span>
+                        <span class="back"></span>
+                    </div> 
+                </div>
+            </section>
         )
     }
 }
