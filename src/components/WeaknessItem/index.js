619e917e8339ebb0746637fdef6331236738d3e0
shenyiling <jzx19951028yyjy@163.com>
2018-04-30 20:54:51 +0800

    fix: weaknessItem切换函数式组件

--- src/components/WeaknessItem/index.js
+++ src/components/WeaknessItem/index.js
@@ -3,36 +3,31 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import './style.less';
 
-export default class WeaknessItem extends React.Component {
+const WeaknessItem = ({
+    url,
+    count,
+    time,
+    className
+}) => (
+        <div className={classnames("weakness-item", className)}>
+            <div
+                title={url} 
+                className="url"
+                >{ url }</div>
+            <div className="count">xss漏洞个数 { count } 个</div>
+            <div className="time">{ time }</div>
+        </div>
+    );
 
-    static defaultProps = {
-        className: null
-    }
-
-    static propTypes = {
-        url: PropTypes.string.isRequired,
-        count: PropTypes.number.isRequired,
-        time: PropTypes.string.isRequired,
-        className: PropTypes.string
-    }
-
-    render() {
-        const {
-            url,
-            count,
-            time,
-            className
-        } = this.props;
-
-        return (
-            <div className={classnames("weakness-item", className)}>
-                <div
-                    title={url} 
-                    className="url"
-                    >{ url }</div>
-                <div className="count">xss漏洞个数 { count } 个</div>
-                <div className="time">{ time }</div>
-            </div>
-        );
-    }
+WeaknessItem.defaultProps = {
+    className: null
 }
+
+WeaknessItem.propTypes = {
+    url: PropTypes.string.isRequired,
+    count: PropTypes.number.isRequired,
+    time: PropTypes.string.isRequired,
+    className: PropTypes.string
+}
+
+export default WeaknessItem;
